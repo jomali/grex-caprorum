80f45d7674609f4a56da4448199336d6f7a2b8a5
J. Francisco Martín <jfm.lisaso@gmx.com>
2024-12-27 13:52:47 +0100

    style: cambia estilo del AppBar

--- src/components/engine/AppBar/AppBar.tsx
+++ src/components/engine/AppBar/AppBar.tsx
@@ -6,28 +6,43 @@ import Typography from "@mui/material/Typography";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 
 // FIXME - define correct type
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ElevationScroll = (props: { children: any }) => {
-  const { children } = props;
-  const theme = useTheme();
+// const ElevationScroll = (props: { children: any }) => {
+//   const { children } = props;
+//   const theme = useTheme();
 
+//   const isTriggered = useScrollTrigger({
+//     disableHysteresis: true,
+//     threshold: 0,
+//   });
+
+//   return React.cloneElement(children, {
+//     sx: {
+//       color: theme.palette.text.primary,
+//       backgroundColor: theme.palette.background.default,
+//       borderBottom: "1px solid " + theme.palette.divider,
+//       zIndex: theme.zIndex.appBar,
+
+//       ...(isTriggered && {
+//         backgroundColor: alpha(theme.palette.background.default, 0.1),
+//         backdropFilter: "blur(2px)",
+//       }),
+//     },
+//   });
+// };
+
+const ElevationScroll = (props: any) => {
+  const { children } = props;
+
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
   const isTriggered = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
   });
 
   return React.cloneElement(children, {
-    sx: {
-      color: theme.palette.text.primary,
-      backgroundColor: theme.palette.background.default,
-      borderBottom: "1px solid " + theme.palette.divider,
-      zIndex: theme.zIndex.appBar,
-
-      ...(isTriggered && {
-        backgroundColor: alpha(theme.palette.background.default, 0.1),
-        backdropFilter: "blur(2px)",
-      }),
-    },
+    elevation: isTriggered ? 2 : 0,
   });
 };
 
@@ -37,7 +52,12 @@ const AppBar = (props: { title: string }) => {
   return (
     <>
       <ElevationScroll>
-        <MuiAppBar elevation={0} position="fixed">
+        <MuiAppBar
+          position="fixed"
+          sx={{
+            color: (theme) => theme.palette.text.primary,
+            backgroundColor: (theme) => theme.palette.background.default,
+          }}>
           <Toolbar>
             <Typography
               sx={{ fontWeight: (theme) => theme.typography.fontWeightBold }}>
